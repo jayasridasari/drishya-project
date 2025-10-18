7ee859b00b8fdf692b151b4a24fec99e57da7cea
jayasridasari <dasarijayasri.22.csm@anits.edu.in>
2025-10-18 20:40:07 +0530

    app constants(roles, statuses,etc.)

---
+++ drishya-backend/src/config/constants.js
@@ -0,0 +1,25 @@
+// src/config/constants.js
+ module.exports = {
+ USER_ROLES: {
+ ADMIN: 'admin',
+ MEMBER: 'member'
+ },
+ TASK_STATUS: {
+ TODO: 'Todo',
+ IN_PROGRESS: 'In Progress',
+ DONE: 'Done'
+ },
+ TASK_PRIORITY: {
+ LOW: 'Low',
+ MEDIUM: 'Medium',
+ HIGH: 'High'
+ },
+ NOTIFICATION_TYPES: {
+ TASK_ASSIGNED: 'task_assigned',
+ TASK_COMPLETED: 'task_completed',
+ TASK_UPDATED: 'task_updated',
+ TASK_COMMENT: 'task_comment',
+ TEAM_TASK: 'team_task',
+ TEAM_ADDED: 'team_added'
+ }
+ };
