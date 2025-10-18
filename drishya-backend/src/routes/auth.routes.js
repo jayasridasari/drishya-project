d4d2462a435f0a6bb31b91ecb983d93b2ae0af3c
jayasridasari <dasarijayasri.22.csm@anits.edu.in>
2025-10-18 20:48:25 +0530

    updated authentication routes

---
+++ drishya-backend/src/routes/auth.routes.js
@@ -0,0 +1,34 @@
+// src/routes/auth.routes.js
+ const express = require('express');
+ const { body } = require('express-validator');
+ const { handleValidation } = require('../middleware/validation');
+ const AuthController = require('../controllers/auth.controller');
+ const router = express.Router();
+ router.post(
+ '/register',
+ body('email').isEmail().normalizeEmail(),
+ body('password').isLength({ min: 8 }),
+ body('role').notEmpty().isIn(['admin','member']),
+ handleValidation,
+ AuthController.register
+ );
+ router.post(
+ '/login',
+ body('email').isEmail().normalizeEmail(),
+ body('password').notEmpty(),
+ handleValidation,
+ AuthController.login
+ );
+ router.post(
+ '/refresh',
+ body('refreshToken').notEmpty(),
+ handleValidation,
+ AuthController.refresh
+ );
+ router.post(
+ '/logout',
+ body('refreshToken').notEmpty(),
+ handleValidation,
+ AuthController.logout
+ );
+module.exports = router;
