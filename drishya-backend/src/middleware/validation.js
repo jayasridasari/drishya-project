b88ccba767cd5ac4449e38459af6df3e7acf6c92
jayasridasari <dasarijayasri.22.csm@anits.edu.in>
2025-10-18 20:42:36 +0530

    created validation error handler

---
+++ drishya-backend/src/middleware/validation.js
@@ -0,0 +1,10 @@
+ // src/middleware/validation.js
+ const { validationResult } = require('express-validator');
+ const handleValidation = (req, res, next) => {
+ const errors = validationResult(req);
+ if (!errors.isEmpty()) {
+ return res.status(400).json({ errors: errors.array() });
+ }
+ next();
+ };
+ module.exports = { handleValidation };
