6d5b165e6e1f584401bdb6718a62447c580cb002
jayasridasari <dasarijayasri.22.csm@anits.edu.in>
2025-10-19 13:02:35 +0530

    added css

---
+++ drishya-frontend/src/store/authStore.js
@@ -0,0 +1,12 @@
+import { create } from 'zustand';
+
+export const useAuthStore = create((set) => ({
+  user: null,
+  token: null,
+  setUser: (user) => set({ user }),
+  setToken: (token) => set({ token }),
+  logout: () => {
+    localStorage.removeItem('refreshToken');
+    set({ user: null, token: null });
+  }
+}));
