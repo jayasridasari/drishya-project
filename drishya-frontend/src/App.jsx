8115d914215660203dbea4fd31f4c4a64a948a7f
jayasridasari <dasarijayasri.22.csm@anits.edu.in>
2025-10-19 12:15:21 +0530

    added more navigations to app.jsx

--- drishya-frontend/src/App.jsx
+++ drishya-frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
+import Layout from './components/layout/Layout';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
@@ -9,22 +11,31 @@ import TaskDetails from './pages/TaskDetails';
 import TeamManagement from './pages/TeamManagement';
 import UserManagement from './pages/UserManagement';
 import Profile from './pages/Profile';
-// ...Other pages
+import PrivateRoute from './routes/PrivateRoute';
+import AdminRoute from './routes/AdminRoute';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Navbar, Sidebar, Notifications, etc. go here */}
+      <ToastContainer />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/kanban" element={<KanbanBoard />} />
-        <Route path="/tasks" element={<TaskList />} />
-        <Route path="/tasks/:id" element={<TaskDetails />} />
-        <Route path="/teams" element={<TeamManagement />} />
-        <Route path="/users" element={<UserManagement />} />
-        <Route path="/profile" element={<Profile />} />
+
+        {/* Protected routes */}
+        <Route element={<PrivateRoute />}>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/kanban" element={<KanbanBoard />} />
+            <Route path="/tasks" element={<TaskList />} />
+            <Route path="/tasks/:id" element={<TaskDetails />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route element={<AdminRoute />}>
+              <Route path="/teams" element={<TeamManagement />} />
+              <Route path="/users" element={<UserManagement />} />
+            </Route>
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
